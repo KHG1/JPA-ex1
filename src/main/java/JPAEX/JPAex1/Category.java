263e4b1c3746f33b85953d2b22edf8b3d821ba2b
김현겸 <rlagusrua7@gmail.com>
2022-03-04 21:47:25 +0900

    JPAex

--- src/main/java/JPAEX/JPAex1/Category.java
+++ src/main/java/JPAEX/JPAex1/Category.java
@@ -6,6 +6,8 @@ import javax.persistence.*;
 import java.util.ArrayList;
 import java.util.List;
 
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 public class Category {
     @Id @GeneratedValue
@@ -13,7 +15,7 @@ public class Category {
 
     private String name;
 
-    @ManyToOne
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "PARENT_ID")
     private Category parent;
 
